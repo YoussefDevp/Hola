5981b81e8dd82ade1feeb88dcc8208d79968b2f8
YoussefDevp <youssefmed212@gmail.com>
2021-01-13 23:59:27 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function login() {
+
+    var  emailElement = document.getElementById('email').value;
+    var  passwordElement = document.getElementById('password').value;
+
+    document.getElementById('textEmail').innerHTML = emailElement;
+    document.getElementById('textPassword').innerHTML = passwordElement;
+    
+
+}
